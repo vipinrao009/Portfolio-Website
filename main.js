e1d4c8219195907ccba4ff865ac4c2dad22f4e74
vipinrao009 <vipinrao2018@gmail.com>
2024-05-03 03:39:54 +0530

    Completed portfolio website

---
+++ main.js
@@ -0,0 +1,59 @@
+// Toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+//section 
+let section = document.querySelector('section')
+let navLink = document.querySelector('header nav a')
+
+window.scroll = ()=> {
+    section.forEach(sec => {
+
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLink.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active')
+            })
+        }
+    })
+
+    // sticy navbar
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky',window.scrollY > 100)
+
+    //remove toggle icon and navbar 
+    menuIcon.classList.remove('fa-xmark')
+    navbar.classList.remove('active')
+};
+
+// Scroll reveal 
+
+ScrollReveal({
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, heading', {origin:'top'});
+ScrollReveal().reveal('.home-image, .services-containor, .portfolio-box, .contact form', {origin:''});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
+
+let typed = new Typed('.multiple-text',{
+    strings:['FullStack Developer','Frontend Developer','Backend Developer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true
+})
